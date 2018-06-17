1816cc2961b45b6305d1016ed3ffa14f28ec8edf
Fabrizzio RIvera <frivera@nevicad.com>
2018-06-16 19:38:42 -0600

    v.0.1-beta1
    Beta version of SalmonPier BackEnd, paired with v0.5-beta1 of the app.

---
+++ app.js
@@ -0,0 +1,112 @@
+﻿var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var sql = require("mssql");
+
+
+// Database config
+var config = {
+    user: 'sa',
+    password: '1234',
+    server: 'localhost',
+    database: 'ENCUESTAS'
+};
+
+var connected
+
+http.listen(8080, function () {
+    console.log("NODE PORT ON *:8080");
+});
+
+//User IDs
+var IDs = [];
+
+io.on("connection", function (socket) {
+    //Log connections and disconnects from Socket Server
+    console.log("-connection-");
+    socket.on('disconnect', function () {
+        console.log("-end of connection-")
+    });
+
+
+    //GetForm event Handler
+    socket.on('getForm', function (queryVals, transactionID) {
+        //Form the query from the values sent by the client
+        query = "SELECT QUESTION.FORM_NAME, TYPE_ID, DATE_CREATED, QUESTION, OPTION_CAPTION, OPTION_VALUE, QUESTION.QUESTION_ID FROM QUESTION " +
+        "INNER JOIN FORM ON QUESTION.FORM_NAME = FORM.FORM_NAME " +
+        "LEFT JOIN OPTIONS ON QUESTION.QUESTION_ID = OPTIONS.QUESTION_ID " +
+        "WHERE FORM_CODE =" + queryVals;
+
+        //Log of transaction in a clear way
+        console.log('-----------------------------------------');
+        console.log('id: ', transactionID)
+        console.log('getForm: ', query);
+        console.log('-----------------------------------------');
+        
+        transactioner(query, transactionID);
+    });
+
+    //Upload form(s) event handler
+    socket.on('insertFilledForms', function (queryVals, transactionID) {
+        var query = "DECLARE @FILLED_ID INT ";
+        for (var f = 0; f < queryVals.length; f++){
+            query = query + "INSERT INTO FILLED_FORM (FORM_CODE, DATE_FILLED) " +
+            "VALUES ("+ queryVals[f].CODE +", '"+ queryVals[f].FINISHED_DATE +"') " +            
+            "SET @FILLED_ID = (SELECT TOP 1 FILLED_ID FROM FILLED_FORM ORDER BY DATE_FILLED) "
+
+            for (var i = 0; i < queryVals[f].QUESTIONS.length; i++){
+
+                query = query + "INSERT INTO ANSWER (VALUE, QUESTION_ID, FILLED_ID) " +
+                "VALUES ('" + queryVals[f].QUESTIONS[i].ANSWER + "', " + queryVals[f].QUESTIONS[i].QUESTION_ID + ", @FILLED_ID) "
+            }
+        }        
+        
+        console.log('-----------------------------------------');
+        console.log('id: ', transactionID)
+        console.log('insertFilledForms: ', query);
+        console.log('-----------------------------------------')
+        
+        transactioner(query, transactionID);
+    });
+});
+
+
+
+function transactioner(query, transactionID) {
+    //Standard MSSQL 4.1 new connection    
+    var conn = new sql.ConnectionPool(config);
+    
+    //Start the connection
+    conn.connect().then(function () {
+        //New request with query formed in the event listener
+        var req = new sql.Request(conn);
+        req.query(query).then(function (recordset) { 
+            //If query returns a row then it will emit
+            //the results with the TransactionID.
+            //(TransactionID is generated randomly from
+            //the client side when the transaction is requested)
+            console.log("DONE ID: " + transactionID)
+            io.sockets.emit(transactionID, {"success": true, "data": recordset});
+            //Log the transaction result
+            console.log("SUCCESFULL TRANSACTION");
+            //And close the SQL connection once the transaction is over
+            conn.close();
+        })
+        .catch(function (err) {
+            //If there is a problem from the SQL server
+            //then emit the error back to client
+            io.sockets.emit(transactionID, {"success": false, "data": err});
+            //Log the transaction result
+            console.log("QUERY ERROR");
+            //And close the SQL connection once the transaction is over
+            conn.close();
+        });
+    })
+    .catch(function (err) {
+        //If sql connection error emit it back to client
+        io.sockets.emit(transactionID, {"success": false, "data": err});
+        //Log the transaction result
+        console.log("CONNECTION ERROR");
+        conn.close();
+    });
+}
